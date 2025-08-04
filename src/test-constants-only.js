3146c2ce44334f8898a43cbd61e3069e90b55f7c
LordErl <erlivanei@gmail.com>
2025-08-04 09:28:45 -0300

    produtos mudança de index para centralizado 02

---
+++ src/test-constants-only.js
@@ -0,0 +1,7 @@
+// Teste apenas de constantes
+import { TABLES, ORDER_STATUS } from './lib';
+
+console.log('=== TESTE DE CONSTANTES ===');
+console.log('TABLES:', TABLES);
+console.log('ORDER_STATUS:', ORDER_STATUS);
+console.log('=== TESTE CONCLUÍDO ===');
