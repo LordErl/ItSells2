727bdf8aa424cd9ed9f54ac279fc1efe34484c9b
LordErl <erlivanei@gmail.com>
2025-08-04 09:28:07 -0300

    produtos mudança de index para centralizado 01

---
+++ src/test-basic.js
@@ -0,0 +1,4 @@
+// Teste básico de importações
+console.log('=== TESTE BÁSICO DE IMPORTAÇÕES ===');
+console.log('Verificando se o script consegue ser executado');
+console.log('Teste concluído com sucesso!');
